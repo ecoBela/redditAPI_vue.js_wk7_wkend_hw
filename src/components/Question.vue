54370d0af086bfc73037736523e0ae25fdb14674
Bela <belahamid@gmail.com>
2021-01-17 16:32:28 +0000

    QuestionDetails component is now connected to the app

--- src/components/Question.vue
+++ src/components/Question.vue
@@ -1,13 +1,18 @@
 <template>
     <div>
         <li>Individual question rendered here</li>
+        <question-details></question-details>
     </div>
   
 </template>
 
 <script>
+import QuestionDetails from './QuestionDetails.vue';
 export default {
     name: 'question',
+    components: {
+        "question-details": QuestionDetails
+    }
 
 }
 </script>
--- src/components/QuestionDetails.vue
+++ src/components/QuestionDetails.vue
@@ -1,9 +1,13 @@
 <template>
+    <div>
+        <p> Additional details will render here </p>
+    </div>
   
 </template>
 
 <script>
 export default {
+    name: 'question-details',
 
 }
 </script>
